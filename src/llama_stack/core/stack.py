4a5ef652868f89f0a0b7769f70c52c4f67f11ecc
Sébastien Han <seb@redhat.com>
2025-11-04 01:12:06 +0100

    chore!: remove SDG API (#4035)
    # What does this PR do?
    
    This API hasn't received any traction and close to zero interest from
    the community. Let's revisit in the future if things change.
    
    Signed-off-by: Sébastien Han <seb@redhat.com>
    Co-authored-by: Ashwin Bharambe <ashwin.bharambe@gmail.com>

--- src/llama_stack/core/stack.py
+++ src/llama_stack/core/stack.py
@@ -31,7 +31,6 @@ from llama_stack.apis.safety import Safety
 from llama_stack.apis.scoring import Scoring
 from llama_stack.apis.scoring_functions import ScoringFunctions
 from llama_stack.apis.shields import Shields
-from llama_stack.apis.synthetic_data_generation import SyntheticDataGeneration
 from llama_stack.apis.tools import RAGToolRuntime, ToolGroups, ToolRuntime
 from llama_stack.apis.vector_io import VectorIO
 from llama_stack.core.conversations.conversations import ConversationServiceConfig, ConversationServiceImpl
@@ -66,7 +65,6 @@ class LlamaStack(
     Agents,
     Batches,
     Safety,
-    SyntheticDataGeneration,
     Datasets,
     PostTraining,
     VectorIO,
